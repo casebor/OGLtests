1128d1e4e13dea12c2fd04a35e75811ad94a2e3e
casebor <carseq@amu.edu.pl>
2022-05-02 19:53:17 +0200

    Cleaning

--- camera.py
+++ camera.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
 #  camera.py
@@ -22,70 +22,8 @@
 #  
 #  
 
-
 import math
 import numpy as np
-import ctypes
-
-
-def my_gluLookAt(in_matrix, eye, target, up):
-    forward = target - eye
-    forward = forward/np.linalg.norm(forward)
-    side = np.cross(forward, up)
-    side = side/np.linalg.norm(side)
-    up = np.cross(side, forward)
-    temp_matrix = np.identity(4, dtype=np.float32)
-    temp_matrix[:3,0] = side
-    temp_matrix[:3,1] = up
-    temp_matrix[:3,2] = -forward
-    #result_matrix = my_glMultiplyMatricesf(in_matrix, temp_matrix)
-    #result_matrix = my_glTranslatef(result_matrix, -eye)
-    result_matrix = my_glTranslatef(temp_matrix, -eye)
-    return result_matrix
-    #view_matrix = np.identity(4, dtype=np.float32)
-    #forward = target - eye
-    #forward = forward/np.linalg.norm(forward)
-    #side = np.cross(up, forward)
-    #side = side/np.linalg.norm(side)
-    #up = np.cross(forward, side)
-    #up = up/np.linalg.norm(up)
-    #dot_x = -np.dot(side, eye)
-    #dot_y = -np.dot(up, eye)
-    #dot_z = -np.dot(forward, eye)
-    #view_matrix[:3,0] = side
-    #view_matrix[:3,1] = up
-    #view_matrix[:3,2] = -forward
-    #view_matrix[3,0] = dot_x
-    #view_matrix[3,1] = dot_y
-    #view_matrix[3,2] = dot_z
-    #return view_matrix
-
-def my_gluPerspectived(fovy, aspect, z_near, z_far):
-    y_max = np.float64(z_near*math.tan(fovy*math.pi/360.0))
-    x_max = np.float64(y_max*aspect)
-    return my_gluFrustumd(-x_max, x_max, -y_max, y_max, z_near, z_far)
-
-def my_gluFrustumd(left, rigth, bottom, top, near, far):
-    frust = np.zeros((4,4), dtype=np.float64)
-    frust[0,0] = np.float64((2*near)/(rigth-left))
-    frust[1,1] = np.float64((2*near)/(top-bottom))
-    frust[2,0] = np.float64((rigth+left)/(rigth-left))
-    frust[2,1] = np.float64((top+bottom)/(top-bottom))
-    frust[2,2] = np.float64((-far-near)/(far-near))
-    frust[2,3] = np.float64(-1)
-    frust[3,2] = np.float64((-2*near*far)/(far-near))
-    return frust
-
-def my_gluFrustumf(left, rigth, bottom, top, near, far):
-    frust = np.zeros((4,4), dtype=np.float32)
-    frust[0,0] = np.float32((2*near)/(rigth-left))
-    frust[1,1] = np.float32((2*near)/(top-bottom))
-    frust[2,0] = np.float32((rigth+left)/(rigth-left))
-    frust[2,1] = np.float32((top+bottom)/(top-bottom))
-    frust[2,2] = np.float32((-far-near)/(far-near))
-    frust[2,3] = np.float32(-1)
-    frust[3,2] = np.float32((-2*near*far)/(far-near))
-    return frust
 
 def my_glRotateXf(in_matrix, angle):
     angle = angle*math.pi/180.0
@@ -114,9 +52,17 @@ def my_glRotateZf(in_matrix, angle):
     rot_matrix[1,1] =  math.cos(angle)
     return my_glMultiplyMatricesf(in_matrix, rot_matrix)
 
-
-#################### FUNCIONANDO ####################
-
+def my_glPerspectivef(fovy, aspect, z_near, z_far):
+    assert(aspect>0)
+    assert(z_far>z_near)
+    f = np.float32(1/(math.tan(fovy*math.pi/180.0)))
+    pers_matrix = np.zeros((4,4), dtype=np.float32)
+    pers_matrix[0,0] = f/aspect
+    pers_matrix[1,1] = f
+    pers_matrix[2,2] = (z_near+z_far)/(z_near-z_far)
+    pers_matrix[3,2] = 2*z_near*z_far/(z_near-z_far)
+    pers_matrix[2,3] = -1
+    return pers_matrix
 
 def my_glScalef(in_matrix, x_scale, y_scale, z_scale):
     x_scale = np.float32(x_scale)
@@ -130,7 +76,6 @@ def my_glScalef(in_matrix, x_scale, y_scale, z_scale):
 
 def my_glTranslatef(orig_matrix, position):
     trans_matrix = np.identity(4, dtype=np.float32)
-    #trans_matrix[:3,3] = position
     trans_matrix[3,:3] = position
     return my_glMultiplyMatricesf(orig_matrix, trans_matrix)
 
@@ -157,11 +102,11 @@ def my_glMultiplyMatricesf(mat1, mat2):
     result[3,3] = mat1[3,0]*mat2[0,3]+mat1[3,1]*mat2[1,3]+mat1[3,2]*mat2[2,3]+mat1[3,3]*mat2[3,3]
     return result
 
-def my_glRotatef(in_matrix, angle, x, y, z):
+def my_glRotatef(in_matrix, angle, dir_vec):
+    vector = np.array(dir_vec, dtype=np.float32)
+    assert(np.linalg.norm(vector)>0.0)
     angle = angle*math.pi/180.0
-    vector = np.array([x,y,z], dtype=np.float32)
-    vector = vector/np.linalg.norm(vector)
-    x,y,z = vector
+    x,y,z = vector/np.linalg.norm(vector)
     c = math.cos(angle)
     s = math.sin(angle)
     rot_matrix = np.identity(4, dtype=np.float32)
@@ -176,15 +121,81 @@ def my_glRotatef(in_matrix, angle, x, y, z):
     rot_matrix[2,2] = z*z*(1-c)+c
     return my_glMultiplyMatricesf(in_matrix, rot_matrix)
 
-def my_gluPerspectivef(in_matrix, fovy, aspect, z_near, z_far):
-    assert(aspect>0)
-    assert(z_far>z_near)
-    f = np.float32(1/(math.tan(fovy*math.pi/180.0)))
-    pers_matrix = np.zeros((4,4), dtype=np.float32)
-    pers_matrix[0,0] = f/aspect
-    pers_matrix[1,1] = f
-    pers_matrix[2,2] = (z_near+z_far)/(z_near-z_far)
-    pers_matrix[3,2] = 2*z_near*z_far/(z_near-z_far)
-    pers_matrix[2,3] = -1
-    return my_glMultiplyMatricesf(in_matrix, pers_matrix)
-    
+def unit_vector(vector):
+    """ Returns the unit vector of the vector.
+    """
+    return vector / np.linalg.norm(vector)
+
+def get_angle(vecA, vecB):
+    """ Return the angle in degrees of two vectors.
+    """
+    vecA_u = unit_vector(vecA)
+    vecB_u = unit_vector(vecB)
+    return np.degrees(np.arccos(np.clip(np.dot(vecA_u, vecB_u), -1.0, 1.0)))
+
+def get_euclidean(pa, pb):
+    """ Returns the distance between two points in R3
+    """
+    import math
+    if int(len(pa)) == 1:
+        pa = [pa[0], 0.0, 0.0]
+    if int(len(pa)) == 2:
+        pa = [pa[0], pa[1], 0.0]
+    if int(len(pb)) == 1:
+        pb = [pb[0], 0.0, 0.0]
+    if int(len(pb)) == 2:
+        pb = [pb[0], pb[1], 0.0]
+    return math.sqrt((pb[0]-pa[0])**2 + (pb[1]-pa[1])**2 + (pb[2]-pa[2])**2)
+
+def get_xyz_coords(xyz_mat):
+    """ Returns the x, y, z position contained in the xyz_mat matrix. The
+        input matrix needs to be a 4x4 matrix.
+    """
+    assert(xyz_mat.ndim==2)
+    assert(xyz_mat.size==16)
+    rot_mat = xyz_mat[:3,:3]
+    pos = -xyz_mat[3,:3]
+    position = pos.dot(rot_mat)
+    return position
+
+#################### NOT RELIABLE FUNCTIONS, NEEDS REVISION ####################
+
+#def my_gluLookAt(in_matrix, eye, target, up):
+    #forward = target - eye
+    #forward = forward/np.linalg.norm(forward)
+    #side = np.cross(forward, up)
+    #side = side/np.linalg.norm(side)
+    #up = np.cross(side, forward)
+    #temp_matrix = np.identity(4, dtype=np.float32)
+    #temp_matrix[:3,0] = side
+    #temp_matrix[:3,1] = up
+    #temp_matrix[:3,2] = -forward
+    #result_matrix = my_glTranslatef(temp_matrix, -eye)
+    #return result_matrix
+
+#def my_gluPerspectived(fovy, aspect, z_near, z_far):
+    #y_max = np.float64(z_near*math.tan(fovy*math.pi/360.0))
+    #x_max = np.float64(y_max*aspect)
+    #return my_gluFrustumd(-x_max, x_max, -y_max, y_max, z_near, z_far)
+
+#def my_gluFrustumd(left, right, bottom, top, near, far):
+    #frust = np.zeros((4,4), dtype=np.float64)
+    #frust[0,0] = np.float64((2*near)/(right-left))
+    #frust[1,1] = np.float64((2*near)/(top-bottom))
+    #frust[2,0] = np.float64((right+left)/(right-left))
+    #frust[2,1] = np.float64((top+bottom)/(top-bottom))
+    #frust[2,2] = np.float64((-far-near)/(far-near))
+    #frust[2,3] = np.float64(-1)
+    #frust[3,2] = np.float64((-2*near*far)/(far-near))
+    #return frust
+
+#def my_gluFrustumf(left, right, bottom, top, near, far):
+    #frust = np.zeros((4,4), dtype=np.float32)
+    #frust[0,0] = np.float32((2*near)/(right-left))
+    #frust[1,1] = np.float32((2*near)/(top-bottom))
+    #frust[2,0] = np.float32((right+left)/(right-left))
+    #frust[2,1] = np.float32((top+bottom)/(top-bottom))
+    #frust[2,2] = np.float32((-far-near)/(far-near))
+    #frust[2,3] = np.float32(-1)
+    #frust[3,2] = np.float32((-2*near*far)/(far-near))
+    #return frust
